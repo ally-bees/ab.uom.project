87b3c6ebda519df79dc12912494b1e63d0744b51
hamsacumar <hamsakumar1010@gmail.com>
2025-03-16 01:42:54 +0530

    audit page with footer,nav,sidebar

--- src/app/pages/auditdashboard/auditdashboard.component.ts
+++ src/app/pages/auditdashboard/auditdashboard.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { PastauditComponent } from '../../components/pastaudit/pastaudit.component';
 import { AuditsummaryComponent } from '../../components/auditsummary/auditsummary.component';
 import { AuditstatustrackerComponent } from '../../components/auditstatustracker/auditstatustracker.component';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FooterComponent } from '../../footer/footer.component';
 
 @Component({
   selector: 'app-auditdashboard',
   standalone: true,
-  imports: [ PastauditComponent,AuditsummaryComponent,AuditstatustrackerComponent ],
+  imports: [ PastauditComponent,AuditsummaryComponent,AuditstatustrackerComponent,HeaderComponent,SidebarComponent,FooterComponent  ],
   templateUrl: './auditdashboard.component.html',
   styleUrl: './auditdashboard.component.css'
 })
--- src/app/pages/auditpage/auditpage.component.ts
+++ src/app/pages/auditpage/auditpage.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { TaxandfeesummaryComponent } from '../../components/taxandfeesummary/taxandfeesummary.component';
 import { TaxandfeetableComponent } from '../../components/taxandfeetable/taxandfeetable.component';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FooterComponent } from '../../footer/footer.component';
 
 @Component({
   selector: 'app-auditpage',
   standalone: true,
-  imports: [  TaxandfeesummaryComponent,TaxandfeetableComponent ],
+  imports: [  TaxandfeesummaryComponent,TaxandfeetableComponent,HeaderComponent,SidebarComponent,FooterComponent ],
   templateUrl: './auditpage.component.html',
   styleUrl: './auditpage.component.css'
 })
