4e24d7d24f98b6c1dbf9b8920f45706f14f0bb4b
lakshan-roshana <lakshanroshana21@gmail.com>
2025-04-15 22:39:31 +0530

    admin side bar updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,6 +17,7 @@ import { FinanceComponent } from './pages/finance/finance.component';
 import { ScheduleComponent } from './pages/schedule/schedule.component';
 import { ShippingDashboardComponent } from './pages/shipping-dashboard/shipping-dashboard.component';
 import { SalesMainpageComponent } from "./mainpage/sales-mainpage/sales-mainpage.component";
+import { DashboardComponent } from './adminpart/dashboard/dashboard.component';
 
 
 export const routes: Routes = [
@@ -48,5 +49,6 @@ export const routes: Routes = [
   {path: 'finance', component: FinanceComponent},
   {path: 'schedule', component: ScheduleComponent},
   {path: 'shipping', component: ShippingDashboardComponent},
+  {path:'admindashboard',component:DashboardComponent},
   { path: '**', redirectTo: 'dashboard' } // Redirect unknown routes to dashboard
 ];
