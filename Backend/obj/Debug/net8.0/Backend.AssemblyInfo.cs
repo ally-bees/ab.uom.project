51df4321af27ba5206f9d542f044e1a3c521e3ba
lakshan-roshana <lakshanroshana21@gmail.com>
2025-04-23 15:50:57 +0530

    backend auth part

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e36008ccb99989a8815dccd4c368a1500b51422f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e098b9221f28f9fac223208426d12c5379b37d12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
