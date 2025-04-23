731a7726fcce40f8432df1ca7565104a8f73f35d
lakshan-roshana <lakshanroshana21@gmail.com>
2025-04-23 16:40:32 +0530

    roles updated in signup

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e098b9221f28f9fac223208426d12c5379b37d12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51df4321af27ba5206f9d542f044e1a3c521e3ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
