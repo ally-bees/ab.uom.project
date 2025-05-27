e55b61fcd32abc63fab6a97724498c19891263e9
hamsacumar <hamsakumar1010@gmail.com>
2025-05-27 22:52:47 +0530

    completed the 95% of software project

--- src/app/components/retentionanal/table/table.service.ts
+++ src/app/components/retentionanal/table/table.service.ts
@@ -1,35 +1,33 @@
-import { Injectable } from '@angular/core'; //use to inject one component to another component
-import { Observable, of } from 'rxjs'; //observable handle multiple data asynchronously  and create observable from static data
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 interface tableRecords {
-  auditId: string;
+  customer_id: string;
   name: string;
-  value: number;
-  tax: number;
-  netValue: number;
+  active_date: string;
+  estimate_date: string;
+  location: string;
   status: string;
 }
 
-@Injectable({               //how we use injectable
+@Injectable({
   providedIn: 'root'
 })
-
-
 export class tableservice {
-  // Mock data to simulate database response
-  private tableRecords: tableRecords[] = [  //taxRecords is array of object(tax records) and TaxRecord[] is how we create a array of object
-    { auditId: '00001A', name: 'Kumar', value: 1000, tax: 100, netValue: 900, status: 'Paid' }, //every thing is an object
-    { auditId: '00002B', name: 'Kumar', value: 1000, tax: 100, netValue: 900, status: 'Pend' },
-    { auditId: '00003C', name: 'Kumar', value: 1000, tax: 100, netValue: 900, status: 'Paid' },
-    { auditId: '00004D', name: 'Kumar', value: 1000, tax: 100, netValue: 900, status: 'Paid' },
-    { auditId: '00005E', name: 'Kumar', value: 1000, tax: 100, netValue: 900, status: 'pend' },
-    { auditId: '00005E', name: 'Kumar', value: 1000, tax: 100, netValue: 900, status: 'Pend' }
-  ];
-  
-  constructor() { }
-  
-  getTaxRecords(): Observable<tableRecords[]> {   //Observable<TaxRecord[]> is return type. method returns an Observable that emits an array of TaxRecord objects.
-    // Return mock data as an Observable (simulating HTTP request)
-    return of(this.tableRecords); //of() is an RxJS function that creates an Observable from the given data.this.taxRecords stores inside the service
+  private apiUrl = 'http://localhost:5241/Customer/custable';
+
+  constructor(private http: HttpClient) {}
+
+  getRecords(from: string, to: string): Observable<tableRecords[]> {
+    const params = new HttpParams()
+      .set('from', from)
+      .set('to', to);
+
+    return this.http.get<tableRecords[]>(this.apiUrl, { params });
+  }
+
+  getAllRecords(): Observable<tableRecords[]> {
+    return this.http.get<tableRecords[]>(this.apiUrl);
   }
 }
