575590bd4cb10e013b187db190b2487cbe8832c4
Pramudi Samarawickrama <pramupiyumika@gmail.com>
2025-07-23 03:09:56 +0530

    Add sales overview component with routing : for displaying and filtering sales data.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { SalestableComponent } from './pages/salestable/salestable.component';
+
+const routes: Routes = [
+  { path: 'salestable', component: SalestableComponent },
+];
+
+export const AppRoutingModule = {
+  routes,
+}; 
